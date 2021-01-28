5fd57038adcd6d1620d40f5d649c249d0472be1d
inunes2mundos <iurinunes01@gmail.com>
2021-01-28 12:29:02 -0300

    put/delete completado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component';
+import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
@@ -12,7 +14,10 @@ const routes: Routes = [
   {path:'cadastrar', component: SignupComponent},
 
   {path:'inicio', component: InicioComponent},
-  {path:'tema', component: TemaComponent}
+  {path:'tema', component: TemaComponent},
+
+  {path: 'tema-edit/:id', component: TemaEditComponent},
+  {path: 'tema-delete/:id', component: TemaDeleteComponent}
 ];
 
 @NgModule({
